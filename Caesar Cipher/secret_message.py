f2bb7b527b141e842b18e23b33aca6ea08a509d6
AbdulBasit619 <619abh@gmail.com>
2025-12-25 21:27:38 +0500

    Updated secret_message.py

--- Caesar Cipher/secret_message.py
+++ Caesar Cipher/secret_message.py
@@ -1,3 +1,4 @@
+# Example usage of this program
 from caesar_cipher import *
 
 prompt = "Please enter the text you want to Encrypt (leave empty to exit): "
@@ -6,7 +7,5 @@ prompt = "Please enter the text you want to Encrypt (leave empty to exit): "
 my_secret_message = input(prompt)
 
 encrypted_message = Encrypt(my_secret_message, 1).encrypt(5)
-print(encrypted_message)
 
 decrypted_message = Decrypt(encrypted_message, 1).decrypt(5)
-print(decrypted_message)
